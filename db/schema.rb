c53cf139d006ed6cfd8e92ea4fa94764978773b8
Hana Harencarova <h.harencarova@gmail.com>
2020-11-30 20:39:59 +0100

    Add routes for Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,32 +11,30 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_30_191507) do
-
-  create_table "breaks", force: :cascade do |t|
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer "tracker_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'breaks', force: :cascade do |t|
+    t.datetime 'start_time'
+    t.datetime 'end_time'
+    t.integer 'tracker_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.integer "category_id"
+  create_table 'projects', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'category_id'
   end
 
-  create_table "time_trackers", force: :cascade do |t|
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer "project_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'time_trackers', force: :cascade do |t|
+    t.datetime 'start_time'
+    t.datetime 'end_time'
+    t.integer 'project_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
